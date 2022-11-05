9feb1dbc42c3c3a38b7732fde0f79bc9963cf440
CarlosP91 <111247956+CarlosP91@users.noreply.github.com>
2022-11-05 14:20:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HACKATHON2022_POPGIL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
