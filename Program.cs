8daf10f826affa489898997674e41232029017fa
AndyG-03 <71861205+AndyGil-03@users.noreply.github.com>
2022-11-05 16:21:14 -0600

    First Menu

--- Program.cs
+++ Program.cs
@@ -4,9 +4,35 @@ namespace HACKATHON2022_POPGIL
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienbenido, ingrese unicamente el numero del problema que desea ver.");
+            Console.WriteLine("Ejemplo: si quires ver el problema 2, ingresar un 2");
+            string NumProblem = Console.ReadLine();
+
+            if (NumProblem == "1")
+            {
+
+            }
+            else if(NumProblem == "2")
+            {
+
+            }
+            else if (NumProblem == "3")
+            {
+
+            }
+            else if (NumProblem == "4")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Ese problema no existe");
+                Console.WriteLine("Precione enter para continuar");
+                Console.ReadLine();
+                Main();
+            }
         }
     }
 }
