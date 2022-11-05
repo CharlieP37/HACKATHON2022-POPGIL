31dc49831e7f8c5e1bc22e186349c2f2a112393a
AndyG-03 <71861205+AndyGil-03@users.noreply.github.com>
2022-11-05 16:23:04 -0600

    Problem 3 need fix

--- Program.cs
+++ Program.cs
@@ -34,5 +34,76 @@ namespace HACKATHON2022_POPGIL
                 Main();
             }
         }
+
+
+
+
+
+
+        static void Problema3()
+        {
+            Console.Clear();
+            Console.WriteLine("                        Sumatoria factorial");
+            Console.WriteLine("Ingresar un numero: ");
+
+            string EntryUser = Console.ReadLine();
+
+            Regex ValidationEntry = new Regex("^([0-9])*$");
+
+            if (ValidationEntry.IsMatch(EntryUser))
+            {
+                UInt64 Res = FactorialCalculation(Convert.ToUInt64(EntryUser));
+                Console.WriteLine("El factorial de " + EntryUser + " es: " + Convert.ToString(Res));
+                ExitCycle();
+            }
+            else
+            {
+                Console.WriteLine("La entrada es incorrecta, debe de ingresar un numero.");
+                Console.WriteLine("Precione enter para continuar");
+                Console.ReadLine();
+                Problema3();
+            }
+        }
+
+        static UInt64 FactorialCalculation(UInt64 NumberEntry)
+        {
+            UInt64 Solution = 1;
+
+            for (UInt64 i = 1; i < NumberEntry; i++)
+            {
+                Solution = Solution * i;
+            }
+
+            return Solution;
+        }
+
+        static void ExitCycle()
+        {
+            Console.WriteLine("Si desea obtener el factorial de otro número, escribir “si” si desea cerrar el programa escribir “no”.");
+            string CycleString = Console.ReadLine();
+
+            if (CycleString == "si")
+            {
+                Problema3();
+            }
+            else if (CycleString == "no")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                ExitCycle();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
