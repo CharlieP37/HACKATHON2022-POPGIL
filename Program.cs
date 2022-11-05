c650002459df3604ba2edb7a451070b292682fd9
AndyG-03 <71861205+AndyGil-03@users.noreply.github.com>
2022-11-05 16:29:58 -0600

    Fix

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace HACKATHON2022_POPGIL
             }
             else if(NumProblem == "2")
             {
-
+                Problem2();
             }
             else if (NumProblem == "3")
             {
@@ -59,6 +59,45 @@ namespace HACKATHON2022_POPGIL
         }
 
 
+
+        static void Problem2()
+        {
+            Console.WriteLine("Problema No. 2 - Contador de vocales en una cadena");
+            Console.WriteLine("\n");
+            Console.WriteLine("Bienvenido, introduzca una cadena de texto y oprima enter para confirmar.");
+            Console.WriteLine("\n");
+
+            Console.Write("Entrada:");
+
+            var Input = Console.ReadLine();
+
+            int TotalVocals = VocalCounter(Input);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("El total de vocales en el texto ingresado es de: " + TotalVocals);
+        }
+
+        private static int VocalCounter(string Text)
+        {
+            int Counter = 0;
+            char[] Vocals = { 'A', 'E', 'I', 'O', 'U' };
+
+            string Input = Text.ToUpper();
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                for (int j = 0; j < Vocals.Length; j++)
+                {
+                    if (Input[i] == Vocals[j])
+                    {
+                        Counter++;
+                        j = Vocals.Length;
+                    }
+                }
+            }
+
+            return Counter;
+        }
 
 
 
